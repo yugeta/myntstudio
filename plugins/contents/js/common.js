920cecac495db2195e0f7f690d45c4dffbb80335
yugeta.koji <geta1972@gmail.com>
2015-05-24 18:35:29 +0900

    new-button

--- plugins/contents/js/common.js
+++ plugins/contents/js/common.js
@@ -24,6 +24,16 @@
 			js_storage.innerHTML = ls;
 		}
 
+		//new button
+		var new_button = document.getElementById("new-button");
+		if(new_button!=null){
+			new_button.onclick=function(){
+				var url = location.href.split("?");
+				location.href = url[0]+"?menu=maintenance";
+			};
+		}
+
+
 	};
 
 	$$LIB.eventAdd(window,"load",$$.set);
